e20a304d3327a1bf3e9ae9cbce512f6fb891439a
Rafael Alvarado <eduaz2002@gmail.com>
2023-03-28 22:58:30 -0600

    Added About Page
    Added a simple description of the project on the the about page

--- Vuejs-Project/src/components/AboutPage.vue
+++ Vuejs-Project/src/components/AboutPage.vue
@@ -1,3 +1,14 @@
 <template>
-    <p>test ABOUT</p>
+    <h1>ABOUT PAGE</h1>
+    <p>En esta pagina web, se puede ver una demostracion de lo que se puede realizar con Vue.js, que son paginas dinamicas con mayor facilidad.</p>
+    <p>En este caso lo mas importante que tenemos que tomar como aprendizaje de este proyecto, es la manera de como se organiza el proyecto, ya que es en cierta parte muy modular, ademas de como es el sintaxis basico para poder hacer que nuestra pagina tenga bastante logica</p>
+
+    <h2>Esperemos que puedan aprender bastante de Vue.js!</h2>
 </template>
+
+<style scoped>
+h1 {
+    text-align: center;
+    margin-bottom: 25px;
+}
+</style>
